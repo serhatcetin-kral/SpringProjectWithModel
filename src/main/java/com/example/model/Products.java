2a14b3b2d5d8fd7e0c5c921079a7e8d4582b9d2c
serhatcetin-kral <74295409+serhatcetin-kral@users.noreply.github.com>
2025-01-24 10:59:03 -0500

    Update Products.java

--- src/main/java/com/example/model/Products.java
+++ src/main/java/com/example/model/Products.java
@@ -7,22 +7,24 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class Products {
+@Table(name = "products")
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+public class Products{
 	
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
-	@Column(name = "`desc`")
+	//@Column(name = "`desc`")
 	private String description;
 	private String  brand;
 	private int price;
@@ -30,6 +32,66 @@ public class Products {
 	private Date releaseDate;
 	private boolean available;
 	private int quantity;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public Date getReleaseDate() {
+		return releaseDate;
+	}
+	public void setReleaseDate(Date releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+	public boolean isAvailable() {
+		return available;
+	}
+	public void setAvailable(boolean available) {
+		this.available = available;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	@Override
+	public String toString() {
+		return "Products [id=" + id + ", name=" + name + ", description=" + description + ", brand=" + brand
+				+ ", price=" + price + ", category=" + category + ", releaseDate=" + releaseDate + ", available="
+				+ available + ", quantity=" + quantity + "]";
+	}
 	
 	
 	
